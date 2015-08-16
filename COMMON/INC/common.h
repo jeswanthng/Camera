20f6e2410b9bef137ef86e879b8e432d49e988cb
jeswanthng <jeswanthng@gmail.com>
2015-08-17 02:00:24 +0530

    My initial commit

---
+++ COMMON/INC/common.h
@@ -0,0 +1,19 @@
+/**
+ *	 @file  common.h
+ *	 @brief Contains common stuff for project
+ *
+ */
+
+#ifndef COMMOM_H
+#define COMMON_H
+
+typedef unsigned char		UINT8;	
+typedef unsigned short int	UINT16;	
+typedef unsigned int		UINT32;	
+
+typedef signed char			SINT8;	
+typedef signed short int	SINT16;
+typedef signed int			SINT32;
+
+
+#endif
---
+++ DRIVERS/CAMERA/INC/camera.h
@@ -0,0 +1,17 @@
+/**
+ *	 @file  camera.h
+ *	 @brief Functions to interface with the serial digital camera camera
+ *
+ */
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+
+void CameraSnapPic(void);
+void CameraGetSpecificPackage(UINT8 packageNumber);
+
+
+
+#endif
+
